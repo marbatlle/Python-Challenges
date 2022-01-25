b2c8230f18eed634a40febb1063ec02790050b9f
marbatlle <marbatlleperez@gmail.com>
2022-01-25 19:39:52 +0100

    add coderbyte-firstfactorial

---
+++ CoderByte/First-Factorial.py
@@ -0,0 +1,14 @@
+#First Factorial
+## Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+def FirstFactorial(num):
+
+  # code goes here
+  list_nums = list(sorted(range(1,num),reverse=True))
+  res = num
+  for x in list_nums:
+    res = res*x
+  return res
+
+# keep this function call here 
+print(FirstFactorial(input()))
